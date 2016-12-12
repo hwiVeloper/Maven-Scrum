abd23e511ddb23d953e36646c59906d26e147d80
devhwi <zziller03@gmail.com>
2016-12-12 13:38:50 +0900

    header and footer

---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Meta Informations -->
+    <meta http-equiv="Content-Type" context="text/html" charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Title -->
+    <title>MAVEN DAILY SCRUM</title>
+
+    <!-- CSS Libraries -->
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+</head>
+<body>
