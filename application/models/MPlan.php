d1ce577961a887aa15213a13dbb726f8b9f8a3aa
devhwi <zziller03@gmail.com>
2016-12-14 16:02:07 +0900

    move ex files

--- application/models/MPlan.php
+++ application/models/MPlan.php
@@ -3,16 +3,18 @@ class MPlan extends CI_Model {
   function __construct() {
     parent::__construct();
   }
+  
+  function view_plan($data) {
+
+  }
 
   function add_plan($data) {
     $query = $this->db->insert('scrum_plan', $data);
-
     return 1;
   }
 
   function add_plan_info($data) {
     $query = $this->db->insert('scrum_plan_info', $data);
-
     return;
   }
 }
