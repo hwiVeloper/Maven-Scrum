67aa89dc0d600d49debc11affed836303a84dc98
devhwi <zziller03@gmail.com>
2017-01-09 11:35:45 +0900

    everyone's today plan -> prev, next button

--- application/views/header.php
+++ application/views/header.php
@@ -34,7 +34,7 @@
 <head>
   <!-- Meta Informations -->
   <meta http-equiv="Content-Type" context="text/html" charset="utf-8">
-  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
   <!-- Title -->
   <title>MAVEN DAILY SCRUM</title>
