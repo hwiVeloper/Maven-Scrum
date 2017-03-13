8db8d91f5a78adf0729644bd3a812008101a3030
devhwi <zziller03@gmail.com>
2017-03-13 14:35:55 +0900

    대댓글 삭제 구현
    특정 댓글 삭제 시, 하위 댓글을 모두 삭제 한다.

--- application/models/MReply.php
+++ application/models/MReply.php
@@ -16,6 +16,24 @@ class MReply extends CI_Model{
   function delete_reply($id) {
     $this->db->where('reply_id', $id);
     $this->db->delete('scrum_reply');
-    return 1;
+  }
+
+  function get_child_reply($id) {
+    $sql = "SELECT r.reply_id
+            FROM (SELECT @rownum := @rownum + 1 AS rownum
+                       , get_lvl() AS id
+                       , @level AS level
+                  FROM (SELECT @start_with:=$id
+                             , @id:=@start_with
+                             , @level:=0) vars
+                  JOIN scrum_reply
+                  WHERE @id IS NOT NULL) func
+                  JOIN scrum_reply r ON func.id = r.reply_id
+            WHERE plan_date in (SELECT plan_date FROM scrum_plan WHERE r.plan_date = plan_date)
+            AND user_id in (SELECT user_id FROM scrum_user WHERE r.user_id = user_id)
+            ORDER BY rownum, r.reply_id";
+    $query = $this->db->query($sql);
+
+    return $query->result_array();
   }
 }
