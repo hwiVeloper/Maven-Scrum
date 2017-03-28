b29c0648e07a6521ceb7676ffc3b1dd2fcd4d561
Ruden <ruden91@naver.com>
2017-03-29 01:20:05 +0900

    highcharts spider web 구현 (위치 재배치 및 데이터 들어올 때 구현)

--- application/views/header.php
+++ application/views/header.php
@@ -63,6 +63,8 @@
 
   <!-- highcharts cdn -->
   <script src="http://code.highcharts.com/highcharts.js"></script>
+  <script src="https://code.highcharts.com/highcharts-more.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
 
 </head>
 <body>
