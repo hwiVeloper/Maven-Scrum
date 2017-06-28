b5ac18481c191d48a031d77d072a8d1df9437d1c
hwiVeloper <zziller03@gmail.com>
2017-06-28 11:24:07 +0900

    rm

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'date', 'smiley');
+$autoload['helper'] = array('date', 'smiley', 'url');
 
 /*
 | -------------------------------------------------------------------
